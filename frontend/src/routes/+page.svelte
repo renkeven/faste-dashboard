8caecee12f70122b4eebc36483a02281467e8f8a
keven ren <ren.keven@gmail.com>
2024-07-10 01:55:35 +1000

    linting

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,51 +1,52 @@
 <script lang="ts">
-	export const ssr = false;
+    export const ssr = false;
 
-	import { type InputData } from '$lib/interfaces';
-	import ChartComponent from './chart_component.svelte';
+    import { type InputData } from "$lib/interfaces";
+    import ChartComponent from "./chart_component.svelte";
 
-	const data: InputData = {
+    const data: InputData = {
         name: "Example",
         data: [
-		{ x: '2023', y: 10, yMin: 8, yMax: 12 },
-		{ x: '2024', y: 15, yMin: 13, yMax: 17 },
-		{ x: '2025', y: 8, yMin: 6, yMax: 10 },
-		{ x: '2026', y: 12, yMin: 10, yMax: 14 },
-		{ x: '2027', y: 20, yMin: 18, yMax: 22 },
-		{ x: '2028', y: 17, yMin: 15, yMax: 19 },
-		{ x: '2029', y: 13, yMin: 11, yMax: 15 },
-		{ x: '2030', y: 18, yMin: 16, yMax: 20 },
-		{ x: '2031', y: 11, yMin: 9, yMax: 13 },
-		{ x: '2032', y: 16, yMin: 14, yMax: 18 }
-	]};
+            { x: "2023", y: 10, yMin: 8, yMax: 12 },
+            { x: "2024", y: 15, yMin: 13, yMax: 17 },
+            { x: "2025", y: 8, yMin: 6, yMax: 10 },
+            { x: "2026", y: 12, yMin: 10, yMax: 14 },
+            { x: "2027", y: 20, yMin: 18, yMax: 22 },
+            { x: "2028", y: 17, yMin: 15, yMax: 19 },
+            { x: "2029", y: 13, yMin: 11, yMax: 15 },
+            { x: "2030", y: 18, yMin: 16, yMax: 20 },
+            { x: "2031", y: 11, yMin: 9, yMax: 13 },
+            { x: "2032", y: 16, yMin: 14, yMax: 18 }
+        ]
+    };
 
-	// let petData: any = null;
-	// let error: string | null = null;
+    // let petData: any = null;
+    // let error: string | null = null;
 
-	// async function fetchPetData() {
-	// 	try {
-	// 		const response = await fetch('http://0.0.0.0:8080/enter');
+    // async function fetchPetData() {
+    // 	try {
+    // 		const response = await fetch('http://0.0.0.0:8080/enter');
 
-	// 		if (!response.ok) {
-	// 			throw new Error(`HTTP error! status: ${response.status}`);
-	// 		}
+    // 		if (!response.ok) {
+    // 			throw new Error(`HTTP error! status: ${response.status}`);
+    // 		}
 
-	// 		petData = await response.json();
-	// 	} catch (e) {
-	// 		console.error('Error fetching pet data:', e);
-	// 		error = 'Failed to fetch pet data';
-	// 	}
-	// }
+    // 		petData = await response.json();
+    // 	} catch (e) {
+    // 		console.error('Error fetching pet data:', e);
+    // 		error = 'Failed to fetch pet data';
+    // 	}
+    // }
 
-	// onMount(() => {
-	// 	fetchPetData();
-	// });
+    // onMount(() => {
+    // 	fetchPetData();
+    // });
 </script>
 
 <div class="flex flex-col w-full p-2">
-	<div class="flex w-full justify-center items-center">
-		<ChartComponent {data} />
-	</div>
+    <div class="flex w-full justify-center items-center">
+        <ChartComponent {data} />
+    </div>
 </div>
 
 <!-- {#if error}
--- frontend/src/routes/chart_component.svelte
+++ frontend/src/routes/chart_component.svelte
@@ -1,180 +1,180 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { writable } from 'svelte/store';
-	import { type InputData } from '$lib/interfaces.ts';
-	import 'chartjs-adapter-dayjs-4/dist/chartjs-adapter-dayjs-4.esm';
+    import { onMount } from "svelte";
+    import { writable } from "svelte/store";
+    import { type InputData } from "$lib/interfaces.ts";
+    import "chartjs-adapter-dayjs-4/dist/chartjs-adapter-dayjs-4.esm";
 
-	let chartCanvas: HTMLCanvasElement;
-	let Chart: any;
-	let zoomPlugin: any;
+    let chartCanvas: HTMLCanvasElement;
+    let Chart: any;
+    let zoomPlugin: any;
 
-	const chartStore = writable<any | null>(null);
+    const chartStore = writable<any | null>(null);
 
-	export let data: InputData;
+    export let data: InputData;
 
-	const cleanedData = [
-		{
-			label: 'Data',
-			data: data['data'].map((point) => ({ x: point.x, y: point.y })),
-			borderColor: 'rgb(75, 192, 192)',
-			backgroundColor: 'rgb(75, 192, 192)',
-			pointStyle: 'circle',
-			pointRadius: 5,
-			pointHoverRadius: 8,
-			fill: false
-		},
-		{
-			label: 'Upper Bound',
-			data: data['data'].map((point) => ({ x: point.x, y: point.yMax })),
-			borderColor: 'rgba(75, 192, 192, 0.2)',
-			backgroundColor: 'rgba(75, 192, 192, 0.2)',
-			pointRadius: 0,
-			pointHitRadius: 0,
-			borderDash: [10, 10],
-			fill: '+1'
-		},
-		{
-			label: 'Lower Bound',
-			data: data['data'].map((point) => ({ x: point.x, y: point.yMin })),
-			borderColor: 'rgba(75, 192, 192, 0.2)',
-			backgroundColor: 'rgba(75, 192, 192, 0.2)',
-			pointRadius: 0,
-			pointHitRadius: 0,
-			borderDash: [10, 10],
-			fill: false
-		}
-	];
+    const cleanedData = [
+        {
+            label: "Data",
+            data: data["data"].map((point) => ({ x: point.x, y: point.y })),
+            borderColor: "rgb(75, 192, 192)",
+            backgroundColor: "rgb(75, 192, 192)",
+            pointStyle: "circle",
+            pointRadius: 5,
+            pointHoverRadius: 8,
+            fill: false
+        },
+        {
+            label: "Upper Bound",
+            data: data["data"].map((point) => ({ x: point.x, y: point.yMax })),
+            borderColor: "rgba(75, 192, 192, 0.2)",
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            pointRadius: 0,
+            pointHitRadius: 0,
+            borderDash: [10, 10],
+            fill: "+1"
+        },
+        {
+            label: "Lower Bound",
+            data: data["data"].map((point) => ({ x: point.x, y: point.yMin })),
+            borderColor: "rgba(75, 192, 192, 0.2)",
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            pointRadius: 0,
+            pointHitRadius: 0,
+            borderDash: [10, 10],
+            fill: false
+        }
+    ];
 
-	const zoomOptions = {
-		limits: {
-			x: { min: 'original', max: 'original', minRange: 1 }
-		},
-		pan: {
-			enabled: true,
-			modifierKey: 'ctrl'
-		},
-		zoom: {
-			drag: {
-				enabled: true
-			},
-			mode: 'x'
-		}
-	};
+    const zoomOptions = {
+        limits: {
+            x: { min: "original", max: "original", minRange: 1 }
+        },
+        pan: {
+            enabled: true,
+            modifierKey: "ctrl"
+        },
+        zoom: {
+            drag: {
+                enabled: true
+            },
+            mode: "x"
+        }
+    };
 
-	const tooltipOptions = {
-		position: 'nearest',
-		callbacks: {
-			label: function (context: any) {
-				const dataPoint = data['data'][context.dataIndex];
-				if (context.datasetIndex === 0) {
-					console.log(data);
-					return [
-						`Y: ${dataPoint.y.toFixed(2)}`,
-						`Min: ${dataPoint.yMin.toFixed(2)}`,
-						`Max: ${dataPoint.yMax.toFixed(2)}`
-					];
-				}
-				return null;
-			}
-		},
-		filter: function (tooltipItem: any) {
-			return tooltipItem.datasetIndex === 0;
-		}
-	};
+    const tooltipOptions = {
+        position: "nearest",
+        callbacks: {
+            label: function (context: any) {
+                const dataPoint = data["data"][context.dataIndex];
+                if (context.datasetIndex === 0) {
+                    console.log(data);
+                    return [
+                        `Y: ${dataPoint.y.toFixed(2)}`,
+                        `Min: ${dataPoint.yMin.toFixed(2)}`,
+                        `Max: ${dataPoint.yMax.toFixed(2)}`
+                    ];
+                }
+                return null;
+            }
+        },
+        filter: function (tooltipItem: any) {
+            return tooltipItem.datasetIndex === 0;
+        }
+    };
 
-	const resetZoom = () => {
-		chartStore.update((chart) => {
-			if (chart) {
-				chart.resetZoom();
-			}
-			return chart;
-		});
-	};
+    const resetZoom = () => {
+        chartStore.update((chart) => {
+            if (chart) {
+                chart.resetZoom();
+            }
+            return chart;
+        });
+    };
 
-	onMount(async () => {
-		// Dynamically import chart.js and zoom plugin
-		const chartModule = await import('chart.js/auto');
-		Chart = chartModule.default;
-		const zoomModule = await import('chartjs-plugin-zoom');
-		zoomPlugin = zoomModule.default;
-		Chart.register(zoomPlugin);
+    onMount(async () => {
+        // Dynamically import chart.js and zoom plugin
+        const chartModule = await import("chart.js/auto");
+        Chart = chartModule.default;
+        const zoomModule = await import("chartjs-plugin-zoom");
+        zoomPlugin = zoomModule.default;
+        Chart.register(zoomPlugin);
 
-		const ctx = chartCanvas.getContext('2d');
-		if (ctx) {
-			let newChart = new Chart(ctx, {
-				type: 'line',
-				data: {
-					datasets: cleanedData
-				},
-				options: {
-					responsive: true,
-					elements: {
-						line: {
-							tension: 0.3
-						}
-					},
-					plugins: {
-						zoom: zoomOptions,
-						title: {
-							display: true,
-							text: data['name']
-						},
-						legend: {
-							display: false
-						},
-						tooltip: tooltipOptions
-					},
-					transitions: {
-						zoom: {
-							animation: {
-								duration: 100
-							}
-						}
-					},
-					scales: {
-						x: {
-							type: 'timeseries',
-							time: {
-								unit: 'year',
-								round: true,
-								displayFormats: {
-									year: 'YYYY'
-								},
-								tooltipFormat: 'YYYY'
-							},
-							position: 'bottom',
-							title: {
-								display: true,
-								text: 'Year'
-							}
-						},
-						y: {
-							type: 'linear',
-							position: 'left',
-							title: {
-								display: true,
-								text: 'Value'
-							}
-						}
-					}
-				}
-			});
-			chartStore.set(newChart);
-			// Add event listener
-			chartCanvas.addEventListener('dblclick', resetZoom);
-		}
-	});
+        const ctx = chartCanvas.getContext("2d");
+        if (ctx) {
+            let newChart = new Chart(ctx, {
+                type: "line",
+                data: {
+                    datasets: cleanedData
+                },
+                options: {
+                    responsive: true,
+                    elements: {
+                        line: {
+                            tension: 0.3
+                        }
+                    },
+                    plugins: {
+                        zoom: zoomOptions,
+                        title: {
+                            display: true,
+                            text: data["name"]
+                        },
+                        legend: {
+                            display: false
+                        },
+                        tooltip: tooltipOptions
+                    },
+                    transitions: {
+                        zoom: {
+                            animation: {
+                                duration: 100
+                            }
+                        }
+                    },
+                    scales: {
+                        x: {
+                            type: "timeseries",
+                            time: {
+                                unit: "year",
+                                round: true,
+                                displayFormats: {
+                                    year: "YYYY"
+                                },
+                                tooltipFormat: "YYYY"
+                            },
+                            position: "bottom",
+                            title: {
+                                display: true,
+                                text: "Year"
+                            }
+                        },
+                        y: {
+                            type: "linear",
+                            position: "left",
+                            title: {
+                                display: true,
+                                text: "Value"
+                            }
+                        }
+                    }
+                }
+            });
+            chartStore.set(newChart);
+            // Add event listener
+            chartCanvas.addEventListener("dblclick", resetZoom);
+        }
+    });
 
-	// Clean up event listener on component destruction
-	onMount(() => {
-		return () => {
-			if (chartCanvas) {
-				chartCanvas.removeEventListener('dblclick', resetZoom);
-			}
-		};
-	});
+    // Clean up event listener on component destruction
+    onMount(() => {
+        return () => {
+            if (chartCanvas) {
+                chartCanvas.removeEventListener("dblclick", resetZoom);
+            }
+        };
+    });
 </script>
 
 <div class="w-1/2 h-1/2">
-	<canvas bind:this={chartCanvas}></canvas>
+    <canvas bind:this={chartCanvas}></canvas>
 </div>
